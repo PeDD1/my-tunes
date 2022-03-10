72892ed5832ce47dc3e9ceccba02758fcc6e239b
PeDD1 <pedrohr0909@gmail.com>
2022-03-10 16:40:57 -0300

    Login Screen working & routes v6 rework

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
+
+const MIN_USER_NAME_LENGTH = 3;
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      password: '',
+      loading: false,
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ name: event.target.value });
+  }
+
+  handleClick = () => {
+    const { history } = this.props;
+    const { name } = this.state;
+    const user = { name };
+    this.setState({ loading: true }, async () => {
+      await createUser(user);
+      history.push('search');
+    })
+  }
+
   render() {
+    const { handleChange, handleClick } = this;
+    const { name, loading } = this.state;
+    if (loading) return <Loading />;
+
     return (
-      <div>Login</div>
+      <form>
+        <input
+          type="text"
+          onChange={handleChange}
+          placeholder="Nome do Usuário"
+        />
+        <input
+          type="password"
+          onChange={handleChange}
+          placeholder="Senha"
+        />
+        <button
+          type="button"
+          onClick={handleClick}
+          disabled={name.length < MIN_USER_NAME_LENGTH}
+        >
+          Entrar
+        </button>
+      </form>
     )
   }
 }
