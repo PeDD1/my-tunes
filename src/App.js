e390ce6f3225240299f5d100e5ab3516990eee05
PeDD1 <pedrohr0909@gmail.com>
2022-03-10 14:52:25 -0300

    Sorting folders & routes for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './components/Routes';
 import './App.css';
 
 function App() {
   return (
-    
+    <main>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </main>
   );
 }
 
