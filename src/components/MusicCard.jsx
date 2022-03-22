b67cc00688c363f106c9ef8b6e6d6f2075530561
PeDD1 <pedrohr0909@gmail.com>
2022-03-21 22:51:52 -0300

    Done the music card component

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,9 +1,53 @@
-import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-export default class MusicCard extends Component {
+class MusicCard extends Component {
   render() {
+    const {
+      music: { trackName, previewUrl, trackId, artworkUrl30 },
+      showImage,
+      handleChange,
+      checked,
+      label } = this.props;
     return (
-      <div>MusicCard</div>
-    )
+      <div>
+        <hr />
+        <li>
+          <div>
+            {showImage ? <img src={artworkUrl30} alt="capa da música" /> : null}
+            {trackName}
+          </div>
+          <div>
+            <audio
+              src={previewUrl} controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              <code>audio</code>
+              .
+            </audio>
+            <label htmlFor={trackId}>
+              <input
+                name="music"
+                type="checkbox"
+                id={trackId}
+                onChange={handleChange}
+                checked={checked}
+              />
+              <span>{label}</span>
+            </label>
+          </div>
+        </li>
+      </div>
+    );
   }
 }
+
+MusicCard.propTypes = {
+  music: PropTypes.arrayOf(PropTypes.string).isRequired,
+  handleChange: PropTypes.func.isRequired,
+  checked: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+  showImage: PropTypes.bool.isRequired,
+};
+
+export default MusicCard;
