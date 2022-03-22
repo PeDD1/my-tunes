92fdc0d41975a53cfa55684ce90c6e1163657d54
PeDD1 <pedrohr0909@gmail.com>
2022-03-21 22:21:40 -0300

    Album page structured

---
+++ src/components/AlbumDetail.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class AlbumDetail extends Component {
+  render() {
+    return (
+      <div>AlbumDetail</div>
+    )
+  }
+}
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,9 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import AlbumDetail from '../components/AlbumDetail';
+import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
 
 class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+    };
+  }
   render() {
+    const { loading } = this.state;
+
     return (
-      <div>Album</div>
+      <div>
+        <Header />
+        {loading ? <Loading />
+          : (
+            <div>
+              <AlbumDetail />
+              <ul>
+                <MusicCard />
+                <hr />
+              </ul>
+            </div>
+          )
+        }
+      </div>
     )
   }
 }
