ad29d34d5f701f72a9aef2353e32ace1e8d400be
PeDD1 <pedrohr0909@gmail.com>
2022-04-25 15:29:26 -0300

    Bug in favorite screen

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,66 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      favorites: [],
+    }
+  }
+
+  componentDidMount() {
+    this.componentDidMount();
+  }
+
+  onMount = () => {
+    this.setState({ loading: true }, async () => {
+      const favoriteSongs = await getFavoriteSongs();
+      this.setState({ loading: false, favorites: favoriteSongs });
+    });
+  }
+
+  handleChange = ({ target: { id } }) => {
+    const { favorites } = this.state;
+    const favoriteMusic = favorites.find(({ trackId }) => trackId === Number(id));
+    this.setState({ loading: true }, async () => {
+      await removeSong(favoriteMusic);
+      const favoriteSongs = await getFavoriteSongs();
+      this.setState({ loading: false, favorites: favoriteSongs });
+    });
+  }
+
+  isFavorite = ({ trackId }) => {
+    const { favorites } = this.state;
+    return favorites.some((music) => trackId === music.trackId);
+  }
+
   render() {
+    const { loading, favorites } = this.state;
+
     return (
-      <div>Favorites</div>
-    )
+      <div>
+        <Header />
+        {(loading) ? <Loading />
+          : (
+            <div className="favorite-list">
+              <h2>Músicas Favoritas</h2>
+              {favorites.map((favorite) => (
+                <MusicCard
+                  showImage
+                  key={favorite.trackId}
+                  handleChange={this.handleChange}
+                  checked={this.isFavorite(favorite)}
+                  label="Favorita"
+                />
+              ))}
+            </div>)}
+      </div>
+    );
   }
 }
 
